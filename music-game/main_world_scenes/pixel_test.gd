4904db04aa34018a6d0a2aa7500aa246b39e048f
stxrrymuffin <jessica.chen270412@gmail.com>
2025-12-13 00:48:22 -0500

    pixel outline tweaking + adding basic player movements & collision shapes

--- music-game/main_world_scenes/pixel_test.gd
+++ music-game/main_world_scenes/pixel_test.gd
@@ -1,6 +1,6 @@
 extends Node3D
 
-@export var spin_speed: float = 1.0
-
+@export var spin_speed: float = 0.5
+	
 func _process(delta: float) -> void:
 	$SubViewportContainer/SubViewport/CameraRig.rotation.y += spin_speed * delta
---
+++ music-game/main_world_scenes/player.gd
@@ -0,0 +1,43 @@
+extends CharacterBody3D
+
+# How fast the player moves in meters per second.
+@export var speed = 2
+# The downward acceleration when in the air, in meters per second squared.
+@export var fall_acceleration = 3
+
+@export var jump_impulse = 1.5
+
+var target_velocity = Vector3.ZERO
+
+
+func _physics_process(delta):
+	var direction = Vector3.ZERO
+
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_back"):
+		direction.z += 1
+	if Input.is_action_pressed("move_forward"):
+		direction.z -= 1
+		
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		target_velocity.y = jump_impulse
+
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+		# Setting the basis property will affect the rotation of the node.
+		$Pivot.basis = Basis.looking_at(-direction)
+
+	# Ground Velocity
+	target_velocity.x = direction.x * speed
+	target_velocity.z = direction.z * speed
+
+	# Vertical Velocity
+	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
+		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+
+	# Moving the Character
+	velocity = target_velocity
+	move_and_slide()
